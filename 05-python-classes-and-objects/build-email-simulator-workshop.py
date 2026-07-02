41816d9307715bb1ee478196b6a276ac6602d916
SenaEnana <senaad47@gmail.com>
2026-07-02 17:29:14 +0300

    added new attribute

--- 05-python-classes-and-objects/build-email-simulator-workshop.py
+++ 05-python-classes-and-objects/build-email-simulator-workshop.py
@@ -1,9 +1,11 @@
 class Email:
-    def __init__(self, sender, receiver, subject, body):
+    def __init__(self, sender, receiver, subject, body, read):
         self.sender = sender
         self.receiver = receiver
         self.subject = subject
         self.body = body
-email_obj = Email('alice@example.com', 'bob@example.com', 'Hello', 'Hi Bob!')
+        self.read = False
+        
+email_obj = Email('alice@example.com', 'bob@example.com', 'Hello', 'Hi Bob!', False)
 print(email_obj.sender)
 print(email_obj.subject)
