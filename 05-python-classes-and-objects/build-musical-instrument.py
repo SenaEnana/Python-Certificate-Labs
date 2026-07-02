08df6c9caaf9c39defb53adec172ae0cc54aebc8
SenaEnana <senaad47@gmail.com>
2026-07-02 16:02:58 +0300

    instance of class created

--- 05-python-classes-and-objects/build-musical-instrument.py
+++ 05-python-classes-and-objects/build-musical-instrument.py
@@ -2,3 +2,8 @@ class MusicalInstrument:
     def __init__(self, name, instrument_type):
         self.name = name
         self.instrument_type = instrument_type
+
+    
+
+instrument_1 = MusicalInstrument('Oboe', 'woodwind')
+instrument_2 = MusicalInstrument('Trumpet', 'brass')
