85f99aff1f64bef4ab96e47539b50018c4cc4ca4
SenaEnana <senaad47@gmail.com>
2026-07-04 10:22:09 +0300

    Still within the send_email method, call the receive_email method of the receiver's inbox to add email to the receiver's inbox.

--- 05-python-classes-and-objects/build-email-simulator-workshop.py
+++ 05-python-classes-and-objects/build-email-simulator-workshop.py
@@ -16,6 +16,7 @@ class User:
         
     def send_email(self, receiver, subject, body):
         email = Email(sender=self, receiver=receiver, subject=subject, body=body)
+        receiver.inbox.receive_email(email)       
 
 class Inbox:
     def __init__(self):
