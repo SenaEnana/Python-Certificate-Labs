2aa20db743f229456832b69604611b6f48affad2
SenaEnana <senaad47@gmail.com>
2026-07-06 16:44:07 +0300

    Now it's time to add some validation to the __init__ method. At the beginning of the method, create an if statement that checks if either name or level are not instances of str.

--- 06-python-oop-exercise/salary-tracker-workshop.py
+++ 06-python-oop-exercise/salary-tracker-workshop.py
@@ -1,14 +1,16 @@
 class Employee:
     def __init__(self, name, level):
+        if not isinstance(name, str) or not isinstance(level, str):
+            raise TypeError("'name' and 'level' attribute must be of type 'str'.")        
         self._name = name
         self._level = level
-
+        
     def __str__(self):
         return f'{self.name}: {self.level}'
 
     def __repr__(self):
-        return f"Employee('{self.name}', '{self.level}')"    
-    
+        return f"Employee('{self.name}', '{self.level}')"
+
     @property
     def name(self):
         return self._name
