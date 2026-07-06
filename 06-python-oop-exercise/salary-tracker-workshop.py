7848e518ca73f96a079a6054dcb7505ea0eb8224
SenaEnana <senaad47@gmail.com>
2026-07-06 16:24:58 +0300

    Add a __str__ method to the Employee class. Make it return an f-string with the format name: level, replacing name and level with the corresponding attributes. After that, print charlie_brown to the console.

--- 06-python-oop-exercise/salary-tracker-workshop.py
+++ 06-python-oop-exercise/salary-tracker-workshop.py
@@ -3,4 +3,9 @@ class Employee:
         self._name = name
         self._level = level
 
+    def __str__(self):
+        return f'{self._name}: {self._level}' 
+
 charlie_brown = Employee('Charlie Brown', 'trainee')
+print(charlie_brown)  
+
