0e22f52b2caa0f02409430a52a5d22f467e0d7af
SenaEnana <senaad47@gmail.com>
2026-07-06 14:12:30 +0300

    finished the quiz question and got 10 out of 10

--- 05-python-classes-and-objects/class-and-objects-quiz.py
+++ 05-python-classes-and-objects/class-and-objects-quiz.py
@@ -6,7 +6,7 @@
 #     pass
 # self className:
 #     pass
-# class className:
+# class className:///correct answer
 #     pass
 # class className
 #     pass
@@ -15,12 +15,12 @@
 
 # It prevents an object from having duplicate attributes.
 # It sets the value of self to the current object.
-# It initializes objects' attributes upon instantiation.
+# It initializes objects' attributes upon instantiation.///correct answer
 # It sets a shortcut to create an instance of a class.
     
 # 3.What is the difference between an instance attribute and a class attribute?
 
-# Instance attributes belong to a specific object; class attributes belong to a class.
+# Instance attributes belong to a specific object; class attributes belong to a class.////correct answer
 # Instance attributes define methods that can be accessed from an object; class attributes define methods that can be accessed from a class.
 # Instance attributes are set within the __init__ method; class attributes are set within the __class__ method.
 # Instance attributes can be directly accessed from the class; class attributes can only be accessed from an actual instance of the class.
@@ -29,13 +29,13 @@
 
 # spam.menu
 # menu[spam]
-# menu.spam()
+# menu.spam()///correct answer
 # spam.menu()
 
 # 5.Which of the following special methods is called under the hood when an object is printed to the console?
 
 # __string__
-# __str__
+# __str__///correct answer
 # __log__
 # __print__
 
@@ -46,7 +46,7 @@
 
 # print(Menu.dish_of_the_day)
 # SyntaxError
-# spam
+# spam////correct answer
 # AttributeError
 # None
 
@@ -54,7 +54,7 @@
 
 # It's a reference to the module where the class is defined.
 # It's a reference to the class being used.
-# It's a reference to the instance of the class calling the method.
+# It's a reference to the instance of the class calling the method.///orrect answer
 # It's a reference to the object initializer.
 
 # 8.Which of the following correctly creates an instance of a class Person?
@@ -62,7 +62,7 @@
 # new Person
 # Person.new()
 # new Person()
-# Person()
+# Person()////correct answer
 
 # 9.What will be the result of the following code?
 
@@ -75,12 +75,12 @@
 # print(dog.name)
 # AttributeError
 # 3
-# TypeError
+# TypeError///correct answer
 # Pinky
 
 # 10.Which of the following is the correct way to access the name attribute of the dog object?
 
 # dog().name
 # dog.name()
-# dog.name
+# dog.name///correct answer
 # dog.get('name')
