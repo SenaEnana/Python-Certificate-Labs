47622bd159ec33c347cbbce165602cd592cbc617
SenaEnana <senaad47@gmail.com>
2026-07-07 17:08:59 +0300

    At the beginning of the __init__ method, create an if statement that checks if the title is either empty or contains whitespace only (you can use the strip method to build the condition).

--- 06-python-oop-exercise/build-media-catalogue-workshop.py
+++ 06-python-oop-exercise/build-media-catalogue-workshop.py
@@ -1,11 +1,14 @@
 class Movie:
     def __init__(self, title, year, director, duration):
+        if title.strip() == '':
+            raise ValueError('Title cannot be empty')
+        
         self.title = title
         self.year = year
         self.director = director
         self.duration = duration
-
     def __str__(self):
         return f'{self.title} ({self.year}) - {self.duration} min, {self.director}'
-movie1 = Movie('Home', '2026', 'Sena', '1 and half hours')
+
+movie1 = Movie('The Matrix', 1999, 'The Wachowskis', 136)
 print(movie1)
