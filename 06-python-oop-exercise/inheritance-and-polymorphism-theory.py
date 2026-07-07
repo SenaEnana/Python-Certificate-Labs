f98fe22ccf3996af54fc2088aa5dc169651a9831
SenaEnana <senaad47@gmail.com>
2026-07-07 15:10:04 +0300

    first theory exercise

---
+++ 06-python-oop-exercise/inheritance-and-polymorphism-theory.py
@@ -0,0 +1,34 @@
+# Questions
+
+#1. What is the primary purpose of inheritance in object-oriented programming?
+
+
+# To create identical copies of a parent class.
+
+# To allow a child class to reuse and extend attributes/methods from a parent class.
+
+# To make all methods in a class private.
+
+# To prevent code duplication by merging unrelated classes.
+
+# 2.What is multiple inheritance in Python?
+
+
+# A child class inheriting from one parent class.
+
+# A parent class with multiple child classes.
+
+# A child class inheriting from multiple parent classes.
+
+# A class that cannot inherit at all.
+
+#3. How do you make a child class inherit from a parent class in Python?
+
+
+# By calling Parent.inherit(Child).
+
+# By copying all methods manually.
+
+# By using the extends keyword.
+
+# By passing the Parent class as an argument to the Child class.
