25d4d2d06d090c3af05287e7cfe9376cddc36510
SenaEnana <senaad47@gmail.com>
2026-07-08 11:27:10 +0300

    Since you are storing the object causing the exception in the exception object, you can make some use of it.

--- 06-python-oop-exercise/build-media-catalogue-workshop.py
+++ 06-python-oop-exercise/build-media-catalogue-workshop.py
@@ -74,9 +74,12 @@ try:
     series1 = TVSeries('Scrubs', 2001, 'Bill Lawrence', 24, 9, 182)
     catalogue.add(series1)
     series2 = TVSeries('Breaking Bad', 2008, 'Vince Gilligan', 47, 5, 62)
+
     catalogue.add('series2')
+
     print(catalogue)
 except ValueError as e:
     print(f'Validation Error: {e}')
 except MediaError as e:
     print(f'Media Error: {e}')
+    print(f'Unable to add {e.obj}: {type(e.obj)}')
