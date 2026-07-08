8657c973c595e5df182c93a81e8b4afede24028c
SenaEnana <senaad47@gmail.com>
2026-07-08 14:46:26 +0300

    in your __str__ method, add a return type hint of str since this method returns a string representation of the object.

--- 06-python-oop-exercise/build-discount-calculator.py
+++ 06-python-oop-exercise/build-discount-calculator.py
@@ -2,5 +2,5 @@ class Product:
     def __init__(self, name: str, price: float) -> None:
         self.name = name
         self.price = price
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{self.name} - ${self.price}'
