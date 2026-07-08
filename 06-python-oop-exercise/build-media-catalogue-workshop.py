41905e87fffdea7d89551824460cb8285f1f1c4f
SenaEnana <senaad47@gmail.com>
2026-07-08 10:54:17 +0300

    Within the TVSeries class, create a __str__ method that returns a string

--- 06-python-oop-exercise/build-media-catalogue-workshop.py
+++ 06-python-oop-exercise/build-media-catalogue-workshop.py
@@ -27,8 +27,8 @@ class TVSeries(Movie):
         
         self.seasons = seasons
         self.total_episodes = total_episodes
-
-
+    def __str__(self):
+        return f'{self.title} ({self.year}) - {self.seasons} seasons, {self.total_episodes} episodes, {self.duration} min avg, {self.director}'    
 class MediaCatalogue:
     def __init__(self):
         self.items = []
@@ -54,7 +54,9 @@ try:
     catalogue.add(movie1)
     movie2 = Movie('Inception', 2010, 'Christopher Nolan', 148)
     catalogue.add(movie2)
+
     series1 = TVSeries('Scrubs', 2001, 'Bill Lawrence', 24, 9, 182)
+
     print(catalogue)
 except ValueError as e:
     print(f'Validation Error: {e}')
