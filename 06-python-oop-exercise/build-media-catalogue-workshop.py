afa5f1eb76eadef676c68a81ef77e3db5aa8531e
SenaEnana <senaad47@gmail.com>
2026-07-08 11:34:55 +0300

    create a method named get_movies and make it return a list containing only instances of the parent class Movie that can be found in self.items.

--- 06-python-oop-exercise/build-media-catalogue-workshop.py
+++ 06-python-oop-exercise/build-media-catalogue-workshop.py
@@ -52,6 +52,8 @@ class MediaCatalogue:
         if not isinstance(media_item, Movie):
             raise MediaError('Only Movie or TVSeries instances can be added', media_item)
         self.items.append(media_item)
+    def get_movies(self):
+        return [item for item in self.items if type(item) is Movie]
 
     def __str__(self):
         if not self.items:
@@ -75,6 +77,7 @@ try:
     catalogue.add(series1)
     series2 = TVSeries('Breaking Bad', 2008, 'Vince Gilligan', 47, 5, 62)
     catalogue.add(series2)
+
     print(catalogue)
 except ValueError as e:
     print(f'Validation Error: {e}')
