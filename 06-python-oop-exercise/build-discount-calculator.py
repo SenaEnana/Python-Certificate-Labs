041d6f7cd8f8a514ff08206f5ce18a4f6e67b3f6
SenaEnana <senaad47@gmail.com>
2026-07-08 14:42:18 +0300

    changed the object based print to text form

--- 06-python-oop-exercise/build-discount-calculator.py
+++ 06-python-oop-exercise/build-discount-calculator.py
@@ -2,3 +2,5 @@ class Product:
     def __init__(self, name: str, price: float):
         self.name = name
         self.price = price
+    def __str__(self):
+        return f'{self.name} - ${self.price}'
