c1475a5ef04e9bdc919bb485d2113f43e22f6c00
SenaEnana <senaad47@gmail.com>
2026-07-08 14:18:48 +0300

    question with answer done completely

--- 06-python-oop-exercise/abstraction-theory-exercise.py
+++ 06-python-oop-exercise/abstraction-theory-exercise.py
@@ -8,13 +8,13 @@
 
 # To prevent methods from being overridden in child classes.
 
-# To hide complex logic and only show essential features
+# To hide complex logic and only show essential features////correct
 
-# How does Python implement abstraction through its ABC module?
+#2 How does Python implement abstraction through its ABC module?
 
-#2 By allowing direct instantiation of abstract classes.
+# By allowing direct instantiation of abstract classes.
 
-# By using decorators and inheritance to define abstract methods that subclasses must implement.
+# By using decorators and inheritance to define abstract methods that subclasses must implement.///correct
 
 # By automatically hiding all methods with double underscores.
 
@@ -24,7 +24,7 @@
 
 # The interface is the engine pistons, and the complex system is the steering wheel.
 
-# The interface is the steering wheel, brakes, and accelerator, and the complex system is the engine, transmission, and braking physics.
+# The interface is the steering wheel, brakes, and accelerator, and the complex system is the engine, transmission, and braking physics.///correct
 
 # The interface is the car manual, and the complex system is the dashboard controls.
 
