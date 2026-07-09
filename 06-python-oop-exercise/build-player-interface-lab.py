34263a7be73d5886bb5797043775bf45a53db934
SenaEnana <senaad47@gmail.com>
2026-07-09 14:21:06 +0300

    You should define an abstract class named Player that inherits from the abc.ABC class.
    The Player class should have an __init__ method that sets:

--- 06-python-oop-exercise/build-player-interface-lab.py
+++ 06-python-oop-exercise/build-player-interface-lab.py
@@ -1,2 +1,10 @@
 import abc
 import random
+
+class Player(abc.ABC):
+    """An abstract class representing a game player."""
+
+    def __init__(self) -> None:
+        self.moves = []
+        self.position = (0, 0)
+        self.path = [self.position]
