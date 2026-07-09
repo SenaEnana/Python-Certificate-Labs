f7b4e2414bfb39ea648721d4b1d86da77847dc79
SenaEnana <senaad47@gmail.com>
2026-07-09 15:30:31 +0300

    with this the project is done and got the following message "Congratulations. Your code passes. Build a Polygon Area Calculator Completed" and the test cases are passed

--- 07-certification-project/build-polygon-area-calculator.py
+++ 07-certification-project/build-polygon-area-calculator.py
@@ -13,12 +13,10 @@ class Rectangle:
     def get_perimeter(self) -> int:
         return 2 * (self.width + self.height)
     def get_diagonal(self) -> float:
-        # Calculates diagonal using the Pythagorean theorem: (width² + height²)**0.5
         return (self.width**2 + self.height**2) ** 0.5
     def get_picture(self) -> str:
         if self.width > 50 or self.height > 50:
             return "Too big for picture."
-        # Creates a row of '*' with a newline, repeated 'height' times
         return ("*" * self.width + "\n") * self.height
 
     def get_amount_inside(self, shape: "Rectangle") -> int:
@@ -45,24 +43,3 @@ class Square(Rectangle):
 
     def __str__(self) -> str:
         return f"Square(side={self.width})"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Congratulations. Your code passes.
-# Build a Polygon Area Calculator
-# Completed
-
