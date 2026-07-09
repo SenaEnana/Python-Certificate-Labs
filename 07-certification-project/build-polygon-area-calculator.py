0965fa3fb7c7830217bcde53bf580ac6c2bc57c5
SenaEnana <senaad47@gmail.com>
2026-07-09 15:28:54 +0300

    code clean up

--- 07-certification-project/build-polygon-area-calculator.py
+++ 07-certification-project/build-polygon-area-calculator.py
@@ -45,7 +45,6 @@ class Square(Rectangle):
         self.set_side(height)
 
     def __str__(self) -> str:
-        # The prompt specifies the width/height are synchronized as the side value
         return f"Square(side={self.width})"
 
 
