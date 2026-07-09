b5175951b1ca1dab2ecb611b55f51cd8dd998f47
SenaEnana <senaad47@gmail.com>
2026-07-09 15:17:58 +0300

    You should create a Rectangle class.
    When a Rectangle object is created, it should be initialized with width and height attributes. The class should also contain the following methods:

---
+++ 07-certification-project/build-polygon-area-calculator.py
@@ -0,0 +1,31 @@
+class Rectangle:
+    """A class representing a rectangle shape."""
+
+    def __init__(self, width: int, height: int) -> None:
+        self.width = width
+        self.height = height
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Congratulations. Your code passes.
+# Build a Polygon Area Calculator
+# Completed
+
