8aca701424b4a8f3fd9821b8fd81c5599617ea1e
SenaEnana <senaad47@gmail.com>
2026-07-10 11:39:58 +0300

    removed some parts

--- 08-linear-data-structure/dynamic-arrys-theory-exercise.py
+++ 08-linear-data-structure/dynamic-arrys-theory-exercise.py
@@ -20,38 +20,7 @@
 
 # New elements are simply discarded if the array is full.
 
-# In which scenario would a static array typically be a more suitable choice than a dynamic array?
-
-#3 When the exact number of elements is unknown and changes frequently.
-
-# When you need to store a very large dataset that might grow indefinitely.
-
-# When you require frequent insertions and deletions at arbitrary positions within the collection.
-
-# When the data size is fixed and known at the time the program is written.Questions
-
-#4 What is the main difference in size between a static array and a dynamic array?
-
-# Static arrays can change their size after being created, while dynamic arrays cannot.
-
-# Static arrays have a fixed size, while dynamic arrays can change size during runtime.
-
-# There is no practical difference in how their sizes are handled.
-
-# Dynamic arrays are always larger than static arrays.
-
-#5 If you need to add more elements to a static array that is already full, what is the typical process involved?
-
-# The static array automatically expands its memory to fit the new elements.
-
-# You must create a new, larger array and copy all existing elements to it.
-
-# The array automatically converts itself into a dynamic array.
-
-# New elements are simply discarded if the array is full.
-
-# In which scenario would a static array typically be a more suitable choice than a dynamic array?
-
+#3 In which scenario would a static array typically be a more suitable choice than a dynamic array?
 
 # When the exact number of elements is unknown and changes frequently.
 
@@ -59,4 +28,4 @@
 
 # When you require frequent insertions and deletions at arbitrary positions within the collection.
 
-# When the data size is fixed and known at the time the program is written.
+# When the data size is fixed and known at the time the program is written.Questions
