2b0a91bb9eba4fd437f309fcfd1ea9fd3cc91a42
SenaEnana <senaad47@gmail.com>
2026-07-10 16:23:41 +0300

    create class Node

---
+++ 09-linked-list-practice-exercise/core-linked-list-example-code.py
@@ -0,0 +1,7 @@
+class Node:
+    """A single node in a linked list."""
+    def __init__(self, data):
+        self.data = data  # Stores the value
+        self.next = None  # Points to the next node (initially None)
+
+
