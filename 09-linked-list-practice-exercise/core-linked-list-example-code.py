c84ba6dbd5fb9018a5fb0657e3f8c9691e66fabb
SenaEnana <senaad47@gmail.com>
2026-07-10 16:32:43 +0300

    code clean up

--- 09-linked-list-practice-exercise/core-linked-list-example-code.py
+++ 09-linked-list-practice-exercise/core-linked-list-example-code.py
@@ -1,29 +1,26 @@
 class Node:
     """A single node in a linked list."""
     def __init__(self, data):
-        self.data = data  # Stores the value
-        self.next = None  # Points to the next node (initially None)
+        self.data = data  
+        self.next = None  
 
 
 class LinkedList:
     """The linked list wrapper class."""
     def __init__(self):
-        self.head = None  # The first node in the list
+        self.head = None  
 
     def append(self, data):
         """Add a new node to the end of the list."""
         new_node = Node(data)
-        
-        # If the list is empty, make the new node the head
+
         if not self.head:
             self.head = new_node
             return      
-        # Otherwise, traverse to the end of the list
         current = self.head
         while current.next:
             current = current.next
         
-        # Link the last node to the new node
         current.next = new_node
 
     def display(self):
@@ -38,28 +35,22 @@ class LinkedList:
 class ExerciseLinkedList(LinkedList):
     def search(self, target):
         current = self.head
-        # TODO: Loop through the list to find the target
-        # Hint: 'while current:' will let you look at every node
+        # TODO: 
         
-        return False # Placeholder
+        return False 
     def delete(self, target):
         current = self.head
         prev = None
 
-       # Case 1: The list is empty
         if not current:
             return
-            
-        # Case 2: The head node itself holds the key to be deleted
         if current.data == target:
             self.head = current.next
             return
-            
-        # Case 3: Search for the key to be deleted, keeping track of the previous node
-        # TODO: Implement the loop to find the node and update 'prev' and 'current'
-        
-        # If the target wasn't found
+
+        # TODO: 
+
         if not current:
             return
             
-        # TODO: Unlink the node from linked list (connect prev.next to current.next)
+        # TODO:
