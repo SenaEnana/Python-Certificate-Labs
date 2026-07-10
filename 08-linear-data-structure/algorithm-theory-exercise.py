74cd37f316568836e62297e33f415ee4fbf7a5c1
SenaEnana <senaad47@gmail.com>
2026-07-10 10:20:32 +0300

    questions pasted

---
+++ 08-linear-data-structure/algorithm-theory-exercise.py
@@ -0,0 +1,34 @@
+# Questions
+
+# Which of the following best describes an algorithm?
+
+
+# A specific programming language used to write code.
+
+# A set of step-by-step instructions designed to solve a problem or perform a task.
+
+# A type of computer hardware component.
+
+# A software application used for developing and playing games.
+
+# What is the primary purpose of Big O notation in the context of algorithms?
+
+
+# To measure the exact time an algorithm takes to run on a specific computer in seconds.
+
+# To count the total number of lines of code in an algorithm.
+
+# To describe how the resource usage of an algorithm grows as the input size increases.
+
+# To determine the best-case performance of an algorithm.
+
+# If an algorithm has a time complexity of O(n), what does this mean about its performance?
+
+
+# The algorithm's running time increases proportionally with the input size.
+
+# The algorithm's running time remains constant regardless of the input size.
+
+# The algorithm's running time grows exponentially with the input size.
+
+# The algorithm's running time decreases as the input size gets larger.
