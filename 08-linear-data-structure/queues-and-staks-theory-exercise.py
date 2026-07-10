b5fe0481758279dcc4edecc6f0c53253071cd63a
SenaEnana <senaad47@gmail.com>
2026-07-10 11:56:54 +0300

    last adjustment

--- 08-linear-data-structure/queues-and-staks-theory-exercise.py
+++ 08-linear-data-structure/queues-and-staks-theory-exercise.py
@@ -1,31 +1,3 @@
 # Questions
 
-#1 What is the primary difference between a stack and a queue?
 
-# Stacks are LIFO, while queues are FIFO.///correct
-
-# Stacks are FIFO, while queues are LIFO.
-
-# Stacks are used for storing data, while queues are used for processing data.
-
-# There is no difference between stacks and queues.
-
-#2 Which operation is used to add an element to a stack?
-
-# push///correct
-
-# pop
-
-# enqueue
-
-# dequeue
-
-#3 Which operation is used to remove an element from a queue?
-
-# push
-
-# pop
-
-# enqueue
-
-# dequeue///correct
