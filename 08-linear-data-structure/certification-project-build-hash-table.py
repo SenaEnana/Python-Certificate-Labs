708d6c23c533120916b82d4929cdbf6d1c573d1b
SenaEnana <senaad47@gmail.com>
2026-07-11 10:10:58 +0300

    The HashTable class should have four instance methods: hash, add, remove, and lookup. The hash method should: Take a string as a parameter. Return a hashed value computed as the sum of the Unicode (ASCII) values of each character in the string. You can use the ord function for this computation.

--- 08-linear-data-structure/certification-project-build-hash-table.py
+++ 08-linear-data-structure/certification-project-build-hash-table.py
@@ -3,4 +3,6 @@ class HashTable:
         # Initialize the collection attribute to an empty dictionary
         self.collection = {}
 
-   
+    def hash(self, key_string):
+        # Return the sum of the Unicode (ASCII) values of each character
+        return sum(ord(char) for char in key_string)
