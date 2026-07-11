9e0f4e172cb546151cada492d770d80137376a69
SenaEnana <senaad47@gmail.com>
2026-07-11 09:12:39 +0300

    Add a method called is_empty that returns self.length == 0.

--- 08-linear-data-structure/build-linked-list-workshop.py
+++ 08-linear-data-structure/build-linked-list-workshop.py
@@ -3,7 +3,9 @@ class LinkedList:
         def __init__(self, element):
             self.element = element
             self.next = None
-
+            
     def __init__(self):
         self.length = 0
-        self.head = None        
+        self.head = None
+    def is_empty(self):
+        return self.length == 0    
