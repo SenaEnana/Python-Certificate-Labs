92b3414562a674c6d2ec86e6efd2f5c95c63d9c6
SenaEnana <senaad47@gmail.com>
2026-07-11 10:21:42 +0300

    edited the instruction

--- 08-linear-data-structure/certification-project-build-hash-table-instructions.py
+++ 08-linear-data-structure/certification-project-build-hash-table-instructions.py
@@ -4,7 +4,8 @@
 #  A hash table works by taking the key as an input and then hashing this key according to a specific hashing function.
 
 # For the purpose of this lab, the hashing function will be simple: 
-# it will sum the Unicode values of each character in the key. The hash value will then be used as the actual key to store the associated value. The same hash value would also be used to retrieve and delete the value associated with the key.
+# it will sum the Unicode values of each character in the key. 
+# The hash value will then be used as the actual key to store the associated value. The same hash value would also be used to retrieve and delete the value associated with the key.
 
 # Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
 
@@ -19,12 +20,14 @@
 # The hash method should:
 
 # Take a string as a parameter.
-# Return a hashed value computed as the sum of the Unicode (ASCII) values of each character in the string. You can use the ord function for this computation.
+# Return a hashed value computed as the sum of the Unicode (ASCII) values of each character in the string. 
+# You can use the ord function for this computation.
 # The add method should:
 
 # Take two arguments representing a key-value pair, and compute the hash of the key.
 # Use the computed hash value as a key to store a dictionary containing the key-value pair inside the collection dictionary.
-# If multiple keys produce the same hash value, their key-value pairs should be stored in the existing nested dictionary under the same hash value.
+# If multiple keys produce the same hash value, their key-value pairs should be stored in the 
+# existing nested dictionary under the same hash value.
 # The remove method should:
 
 # Take a key as its argument and compute its hash.
