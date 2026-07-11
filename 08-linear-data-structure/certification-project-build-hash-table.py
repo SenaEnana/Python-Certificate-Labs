051bd47e5af0c20c27ee6c8f775717bc20b9d051
SenaEnana <senaad47@gmail.com>
2026-07-11 10:16:55 +0300

    last code and hash table project is completed with this and done

--- 08-linear-data-structure/certification-project-build-hash-table.py
+++ 08-linear-data-structure/certification-project-build-hash-table.py
@@ -1,17 +1,13 @@
 class HashTable:
     def __init__(self):
-        # Initialize the collection attribute to an empty dictionary
         self.collection = {}
 
     def hash(self, key_string):
-        # Return the sum of the Unicode (ASCII) values of each character
         return sum(ord(char) for char in key_string)
 
     def add(self, key, value):
-        # Compute the hash value of the key
         hash_value = self.hash(key)
-        
-        # If the hash value doesn't exist in the collection, initialize a nested dict
+
         if hash_value not in self.collection:
             self.collection[hash_value] = {}
 
