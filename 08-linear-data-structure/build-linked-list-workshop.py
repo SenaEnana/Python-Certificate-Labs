b526c620b462c48f238a8a7efd39f859bc02f897
SenaEnana <senaad47@gmail.com>
2026-07-11 09:10:37 +0300

    Inside the LinkedList class, add an __init__ method that sets self.length to 0.

--- 08-linear-data-structure/build-linked-list-workshop.py
+++ 08-linear-data-structure/build-linked-list-workshop.py
@@ -2,4 +2,6 @@ class LinkedList:
     class Node:
         def __init__(self, element):
             self.element = element
-            self.next = None            
+            self.next = None
+    def __init__(self):
+        self.length = 0    
