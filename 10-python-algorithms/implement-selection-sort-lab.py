df7349215c5a85a1a51dd86fcdeb1121ce84ce1e
SenaEnana <senaad47@gmail.com>
2026-07-16 16:46:36 +0300

    Your selection_sort function should modify the input list in-place, and return it once it's sorted.

--- 10-python-algorithms/implement-selection-sort-lab.py
+++ 10-python-algorithms/implement-selection-sort-lab.py
@@ -1,3 +1,7 @@
 def selection_sort(arr):
     n = len(arr)
     
+    # Traverse through all array elements
+    for i in range(n):
+        # Find the minimum element in the remaining unsorted array
+        pass
