c25764bd4977536f0f2be4c44b2721a6a578813b
SenaEnana <senaad47@gmail.com>
2026-07-16 16:48:43 +0300

    clean up

--- 10-python-algorithms/implement-selection-sort-lab.py
+++ 10-python-algorithms/implement-selection-sort-lab.py
@@ -1,7 +1,5 @@
 def selection_sort(arr):
     n = len(arr)
-    
-    # Traverse through all array elements
     for i in range(n):
         # Find the minimum element in the remaining unsorted array
         min_idx = i
