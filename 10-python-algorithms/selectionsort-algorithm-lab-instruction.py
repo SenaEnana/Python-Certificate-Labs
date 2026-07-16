c244b7e91674abe947548c45fc8269aa4b9adcec
SenaEnana <senaad47@gmail.com>
2026-07-16 16:43:12 +0300

    editting

--- 10-python-algorithms/selectionsort-algorithm-lab-instruction.py
+++ 10-python-algorithms/selectionsort-algorithm-lab-instruction.py
@@ -1,9 +1,15 @@
 # Implement the Selection Sort Algorithm
 # Selection sort is another popular sorting algorithm taught in most computer science courses.
 
-# This algorithm works by repeatedly finding the smallest element from the unsorted portion of the list and swapping it with the first unsorted element. It begins by selecting the minimum value in the entire list and swapping it with the first element. Then it moves to the second position, finds the smallest value in the remaining unsorted elements, and swaps it with the second element. This process continues, moving through the list one element at a time, until the entire list is sorted.
+# This algorithm works by repeatedly finding the smallest element from the unsorted portion of 
+# the list and swapping it with the first unsorted element. 
+# It begins by selecting the minimum value in the entire list and swapping it with the first element. 
+# Then it moves to the second position, finds the smallest value in the remaining unsorted elements, and swaps it with the second element. 
+# This process continues, moving through the list one element at a time, until the entire list is sorted.
 
-# Selection sort results in a quadratic time complexity in the best, average, and worst case scenarios. The space complexity will be constant O(1) because the sorting is done in place and a constant amount of memory is being used regardless of the size of the list.
+# Selection sort results in a quadratic time complexity in the best, 
+# average, and worst case scenarios. The space complexity will be constant O(1) 
+# because the sorting is done in place and a constant amount of memory is being used regardless of the size of the list.
 
 # Objective: Fulfill the user stories below and get all the tests to pass to complete the lab.
 
