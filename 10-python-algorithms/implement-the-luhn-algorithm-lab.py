1539f18eb8690fa092505ee3128e378d4456ad0d
SenaEnana <senaad47@gmail.com>
2026-07-16 17:16:17 +0300

    Return VALID! if the card number is valid; otherwise, return INVALID!.

--- 10-python-algorithms/implement-the-luhn-algorithm-lab.py
+++ 10-python-algorithms/implement-the-luhn-algorithm-lab.py
@@ -6,3 +6,6 @@ def verify_card_number(card_number):
     if not cleaned_card.isdigit():
         return "INVALID!"
         
+    total_sum = 0
+    # Reverse the digits to easily process starting from the right (1-based or 0-based index)
+    digits = [int(char) for char in cleaned_card[::-1]]
