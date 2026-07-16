caadf7e3ae5fcdcdf74e11fb9d550c8fa515e8d5
SenaEnana <senaad47@gmail.com>
2026-07-16 15:26:22 +0300

    The square_root_bisection function should: Raise a ValueError with the message Square root of negative number is not defined in real numbers if the number passed to the function is negative

--- 10-python-algorithms/implement-bisection-method-lab.py
+++ 10-python-algorithms/implement-bisection-method-lab.py
@@ -1,3 +1,5 @@
 def square_root_bisection(number, tolerance=1e-7, max_iterations=100):
     # 1. Handle negative numbers
-    pass
+    if number < 0:
+        raise ValueError("Square root of negative number is not defined in real numbers")
+    
