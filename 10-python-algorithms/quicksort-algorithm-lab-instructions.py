bc91feed22e3f357dc5b7e587036401ff0cd1072
SenaEnana <senaad47@gmail.com>
2026-07-16 16:38:00 +0300

    test cases are tested and approved!

--- 10-python-algorithms/quicksort-algorithm-lab-instructions.py
+++ 10-python-algorithms/quicksort-algorithm-lab-instructions.py
@@ -21,3 +21,14 @@
 # Waiting:7. quick_sort([4, 42, 16, 23, 15, 8]) should return [4, 8, 15, 16, 23, 42].
 # Waiting:8. quick_sort([87, 11, 23, 18, 18, 23, 11, 56, 87, 56]) should return [11, 11, 18, 18, 23, 23, 56, 56, 87, 87].
 # Waiting:9. You should not import any module or use built-in sorting methods in your code.
+
+# Tests:
+# Passed:1. You should have a function named quick_sort.
+# Passed:2. Your quick_sort function should take a single parameter.
+# Passed:3. quick_sort([]) should return an empty list.
+# Passed:4. Your quick_sort function should not modify the list passed to it as the argument.
+# Passed:5. quick_sort([20, 3, 14, 1, 5]) should return [1, 3, 5, 14, 20].
+# Passed:6. quick_sort([83, 4, 24, 2]) should return [2, 4, 24, 83].
+# Passed:7. quick_sort([4, 42, 16, 23, 15, 8]) should return [4, 8, 15, 16, 23, 42].
+# Passed:8. quick_sort([87, 11, 23, 18, 18, 23, 11, 56, 87, 56]) should return [11, 11, 18, 18, 23, 23, 56, 56, 87, 87].
+# Passed:9. You should not import any module or use built-in sorting methods in your code.
