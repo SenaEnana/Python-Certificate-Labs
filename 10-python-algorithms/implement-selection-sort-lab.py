57e2a84b289d40d79ddfffb0398b707b2c0b0ec3
SenaEnana <senaad47@gmail.com>
2026-07-16 16:47:10 +0300

    Your selection_sort function should follow the selection sort algorithm,

--- 10-python-algorithms/implement-selection-sort-lab.py
+++ 10-python-algorithms/implement-selection-sort-lab.py
@@ -4,4 +4,4 @@ def selection_sort(arr):
     # Traverse through all array elements
     for i in range(n):
         # Find the minimum element in the remaining unsorted array
-        pass
+        min_idx = i
