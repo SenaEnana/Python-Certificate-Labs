496887b5d0271c90f30a8a2eaa1eb6d3a549785f
SenaEnana <senaad47@gmail.com>
2026-07-16 14:16:02 +0300

    questions pasted

---
+++ 10-python-algorithms/merge-sort-theory-exercise.py
@@ -0,0 +1,34 @@
+# Questions
+
+# What is the divide and conquer paradigm in computer science?
+
+
+# A technique for detecting a cycle in function value iterations using just two iterators.
+
+# An algorithm for comparing two elements and swapping them from smallest to largest if needed.
+
+# A technique for recursively breaking down problems into smaller sub-problems.
+
+# An algorithm to compute the shortest connecting network for points in a plane.
+
+# What is the time complexity for the merge sort algorithm?
+
+
+# O(n log n)
+
+# O(log n²)
+
+# O(n³ log n)
+
+# O(log n³)
+
+# What is the space complexity for the merge sort algorithm?
+
+
+# O(n²)
+
+# O(1)
+
+# O(n log n)
+
+# O(n)
