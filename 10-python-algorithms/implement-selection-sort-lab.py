34a2c5c7d2d5ab6eef02f09723085d3e5926a801
SenaEnana <senaad47@gmail.com>
2026-07-16 16:49:26 +0300

    with this the selection sort algorithm is done "Congratulations. Your code passes. Implement the Selection Sort Algorithm 100% complete"

--- 10-python-algorithms/implement-selection-sort-lab.py
+++ 10-python-algorithms/implement-selection-sort-lab.py
@@ -1,14 +1,12 @@
 def selection_sort(arr):
     n = len(arr)
     for i in range(n):
-        # Find the minimum element in the remaining unsorted array
+
         min_idx = i
         for j in range(i + 1, n):
             if arr[j] < arr[min_idx]:
                 min_idx = j
                 
-        # Swap the found minimum element with the first unsorted element
-        # Only perform the swap if the minimum is not already in the correct position
         if min_idx != i:
             arr[i], arr[min_idx] = arr[min_idx], arr[i]
             
