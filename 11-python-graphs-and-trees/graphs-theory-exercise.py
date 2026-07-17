3308780c0ca5593a5bfcc60078b57df7be35c0f1
SenaEnana <senaad47@gmail.com>
2026-07-17 08:20:21 +0300

    questions ready

---
+++ 11-python-graphs-and-trees/graphs-theory-exercise.py
@@ -0,0 +1,34 @@
+# Questions
+
+# What is a graph in computer science?
+
+
+# A data structure used to represent relationships between objects.
+
+# A mathematical equation.
+
+# A visual representation of data.
+
+# A programming language.
+
+# What are the two main components of a graph?
+
+
+# Points and lines
+
+# Vertices and nodes
+
+# Nodes and edges
+
+# Elements and vertices
+
+# What is a directed graph?
+
+
+# A graph where edges do not have a direction.
+
+# A graph where edges have a direction.
+
+# A graph with more nodes than edges.
+
+# A graph with more edges than nodes.
