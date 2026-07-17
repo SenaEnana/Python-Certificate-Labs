9e91ac303acb5a4930874e95d56e0d8c24b5e6b8
SenaEnana <senaad47@gmail.com>
2026-07-17 16:35:17 +0300

    If we have successfully placed N queens, we found a valid solution

--- 11-python-graphs-and-trees/implement-n-queens-lab.py
+++ 11-python-graphs-and-trees/implement-n-queens-lab.py
@@ -1,11 +1,11 @@
 def dfs_n_queens(n):
-    # 1. Base case: if n is less than 1, return an empty list
     if n < 1:
         return []
     
     solutions = []
-    # The stack will store tuples representing our search state:
-    # (current_board_configuration)
-    # An empty list [] means no queens have been placed yet.
     stack = [[]]
     
+    while stack:
+        current_state = stack.pop()
+        current_row = len(current_state)
+        
