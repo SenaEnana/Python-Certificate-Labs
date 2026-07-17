470c302c36338d760ef345fdb470b6854959f087
SenaEnana <senaad47@gmail.com>
2026-07-17 09:32:46 +0300

    ready questions

---
+++ metrics-and-adjecent-list-theory.py
@@ -0,0 +1,34 @@
+# Questions
+
+# For which of the following scenarios is an adjacency matrix the most efficient choice for representing a graph?
+
+
+# A social network with billions of people and very few connections per person.
+
+# A computer network with only five connections.
+
+# A dense graph where every node is connected to most other nodes.
+
+# A graph where the main operation is to find all neighbors of a specific node.
+
+# When would it be more efficient to use an adjacency list over an adjacency matrix?
+
+
+# When the graph is dense and has many edges.
+
+# When you need to check if an edge exists between two nodes very quickly.
+
+# When the graph has a high number of vertices and many connections.
+
+# When the graph is sparse, with a high number of vertices and a low number of edges.
+
+# Which of the following operations is faster in an adjacency matrix compared to an adjacency list?
+
+
+# Finding all neighbors of a single node.
+
+# Checking if a direct edge exists between two specific nodes.
+
+# Iterating through all nodes in the graph.
+
+# Adding a new node to the graph.
