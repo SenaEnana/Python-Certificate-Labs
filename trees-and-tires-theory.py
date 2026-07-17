cef6b4bf4a6b094e226cf24624e59bdf87cdf830
SenaEnana <senaad47@gmail.com>
2026-07-17 09:43:58 +0300

    ready questions

---
+++ trees-and-tires-theory.py
@@ -0,0 +1,34 @@
+# Questions
+
+# Which of the following statements about a Binary Search Tree (BST) is always true?
+
+
+# All values in the left subtree of a node are less than the node's value.
+
+# Every node can have up to three children.
+
+# The tree is guaranteed to be perfectly balanced after every insertion.
+
+# It is a specialized tree-like structure optimized for storing strings.
+
+# For which of the following tasks is a trie, or prefix tree most effective?
+
+
+# Finding the smallest value in a large dataset.
+
+# Managing a hierarchical file system on a computer.
+
+# Implementing an autocomplete feature.
+
+# Storing an unordered list of unique numbers.
+
+# What is the fundamental difference between a general tree and a Binary Tree?
+
+
+# A Binary Tree can only store numbers, while a general tree can store any data type.
+
+# A Binary Tree has a root node, but a general tree does not.
+
+# A Binary Tree has no parent-child relationships, unlike a general tree.
+
+# A Binary Tree restricts each node to a maximum of two children, while a general tree does not.
