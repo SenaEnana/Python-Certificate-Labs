65fde1d94a6b6120a7da18cfeee8613ed33962a5
SenaEnana <senaad47@gmail.com>
2026-07-17 16:43:35 +0300

    understand and edit

--- 11-python-graphs-and-trees/graphs-and-tree-quiz.py
+++ 11-python-graphs-and-trees/graphs-and-tree-quiz.py
@@ -1,161 +1,121 @@
 # Graphs and Trees Quiz
 # To pass the quiz, you must correctly answer at least 18 of the 20 questions below.
 
-# 1.
- 
-# What is a graph?
+# 1.What is a graph?
 
 # A data structure used to represent the connections or relationships between objects or entities.
 # A linear data structure that follows the Last-In, First-Out principle.
 # A linear data structure that follows the First-In, First-Out principle.
 # A data structure that stores multiple values of the same type that are contiguous in memory.
-# 2.
- 
-# Which of the following is NOT a typical scenario that can be represented using a graph?
+# 2.Which of the following is NOT a typical scenario that can be represented using a graph?
 
 # temperature measurement over time
 # transportation networks
 # social networks
 # communications networks
-# 3.
- 
-# Which of the following terms describes an object or entity that is part of the network depicted by a graph?
+# 3.Which of the following terms describes an object or entity that is part of the network depicted by a graph?
 
 # Node
 # Endpoint
 # CPU
 # Cookie
-# 4.
- 
-# Which of the following terms describes the connections between nodes in a graph?
+# 4.Which of the following terms describes the connections between nodes in a graph?
 
 # Edges
 # Gates
 # Vertices
 # Connectors
-# 5.
- 
-# What is an undirected graph?
+# 5. What is an undirected graph?
 
 # A graph where the edges don't have a specific direction.
 # A graph where the nodes and edges connect at the midpoint.
 # A graph where the edges have a specific direction.
 # A graph where the nodes are undirected but the edges are directed.
-# 6.
- 
-# Which of the following refers to a graph in which each node is associated with a label or identifier in addition to its data?
+# 6.Which of the following refers to a graph in which each node is associated with a label or identifier in addition to its data?
 
 # Virtual labeled graph
 # Vertex labeled graph
 # Version labeled graph
 # Vertical labeled graph
-# 7.
- 
-# Which of the following refers to a directed graph with at least one cycle?
+# 7.Which of the following refers to a directed graph with at least one cycle?
 
 # Cycling graph
 # Circulating graph
 # Circular graph
 # Cyclic graph
-# 8.
- 
-# What is a weighted graph?
+# 8.What is a weighted graph?
 
 # A specific type of edge labeled graph that assigns categories instead of numerical values to edges.
 # A specific type of edge labeled graph in which the labels on the edges represent values that can be compared and used to perform arithmetic operations.
 # A specific type of edge labeled graph that shows only the direction of connections between nodes.
 # A specific type of edge labeled graph that treats all edge weights as equal regardless of label.
-# 9.
- 
-# What is a directed acyclic graph?
+# 9.What is a directed acyclic graph?
 
 # A directed graph with three cycles.
 # A directed graph with one cycle.
 # A directed graph with two cycles.
 # A directed graph with no cycles.
-# 10.
- 
-# Which of the following refers to a graph with two or more groups of nodes that are not connected by any edges?
+# 10.Which of the following refers to a graph with two or more groups of nodes that are not connected by any edges?
 
 # Disconnected graph
 # Disconnection graph
 # Disconnecting graph
 # Disconnect graph
-# 11.
- 
-# What is the Breadth-first search algorithm?
+# 11.What is the Breadth-first search algorithm?
 
 # An algorithm that visits all nodes in one direction before moving another direction in the graph.
 # An algorithm that visits two nodes in one direction before moving another direction in the graph.
 # An algorithm that visits four nodes at a time before moving to the next level in the graph.
 # An algorithm that visits all neighboring nodes before moving to the next level in the graph.
-# 12.
- 
-# Which of the following algorithms will follow each branch as deep as possible before it backtracks?
+# 12.Which of the following algorithms will follow each branch as deep as possible before it backtracks?
 
 # Daemon-First Search
 # Dock-First Search
 # Data-First Search
 # Depth-First Search
-# 13.
- 
-# Which of the following terms represents a two-dimensional list in which the rows and columns represent the graph's vertices?
+# 13.Which of the following terms represents a two-dimensional list in which the rows and columns represent the graph's vertices?
 
 # Adjacency matrix
 # Array matrix
 # Vertex matrix
 # Depth matrix
-# 14.
- 
-# Which of the following terms represents an array or dictionary that stores all the neighbors of each node?
+# 14.Which of the following terms represents an array or dictionary that stores all the neighbors of each node?
 
 # Tuple list
 # Adjacency list
 # Breadth list
 # Cyclic list
-# 15.
- 
-# What must be true for a graph to be classified as a tree?
+# 15.What must be true for a graph to be classified as a tree?
 
 # It must have at least one loop or cycle.
 # It must have three loops or cycles.
 # It cannot have loops or cycles.
 # It must have at least two loops or cycles.
-# 16.
- 
-# Which of the following is a type of tree in which each node can have at most two child nodes?
+# 16.Which of the following is a type of tree in which each node can have at most two child nodes?
 
 # Binary tree
 # Linear tree
 # Spatial tree
 # Depth tree
-# 17.
- 
-# What is a Trie?
+# 17.What is a Trie?
 
 # A tree data structure used to store a set of strings.
 # A tree data structure used to store a set of dictionaries.
 # A tree data structure used to store a set of lists.
 # A tree data structure used to store a set of numbers.
-# 18.
- 
-# How does the priority queue data structure differ from a regular queue data structure?
+# 18.How does the priority queue data structure differ from a regular queue data structure?
 
 # A priority queue automatically removes duplicates before insertion.
 # A priority queue always processes elements in the order they were added.
 # A priority queue processes elements based on their priority.
 # A priority queue only stores elements of the same data type.
-# 19.
- 
-# What are the two types of heaps?
+# 19.What are the two types of heaps?
 
 # Max-heap and Min-heap
 # Horizontal-heap and Vertical-heap
 # Wide-heap and Tall-heap
 # Breadth-heap and Depth-heap
-# 20.
- 
-# What are the average and worst case time complexities for inserting and extracting the minimum or maximum value from a heap?
+# 20.What are the average and worst case time complexities for inserting and extracting the minimum or maximum value from a heap?
 
 # O(n log n)
 # O(n)
