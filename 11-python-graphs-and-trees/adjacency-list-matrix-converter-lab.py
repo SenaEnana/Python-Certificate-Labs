c1d112b55402b3f093502a48038bf81df56c9f94
SenaEnana <senaad47@gmail.com>
2026-07-17 11:30:33 +0300

    The function should: Convert the adjacency list to an adjacency matrix.

--- 11-python-graphs-and-trees/adjacency-list-matrix-converter-lab.py
+++ 11-python-graphs-and-trees/adjacency-list-matrix-converter-lab.py
@@ -2,3 +2,6 @@ def adjacency_list_to_matrix(adj_list):
     # 1. Determine the number of nodes
     # The number of nodes is equal to the number of keys in the adjacency list
     num_nodes = len(adj_list)
+    
+    # 2. Initialize the matrix with all 0s (size num_nodes x num_nodes)
+    matrix = [[0] * num_nodes for _ in range(num_nodes)]
