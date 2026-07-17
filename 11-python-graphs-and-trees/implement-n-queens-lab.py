f96bbd9b8c0e5c5e6738441fe6693694e1bf6873
SenaEnana <senaad47@gmail.com>
2026-07-17 16:34:44 +0300

    An empty list [] means no queens have been placed yet.

--- 11-python-graphs-and-trees/implement-n-queens-lab.py
+++ 11-python-graphs-and-trees/implement-n-queens-lab.py
@@ -4,4 +4,8 @@ def dfs_n_queens(n):
         return []
     
     solutions = []
- 
+    # The stack will store tuples representing our search state:
+    # (current_board_configuration)
+    # An empty list [] means no queens have been placed yet.
+    stack = [[]]
+    
