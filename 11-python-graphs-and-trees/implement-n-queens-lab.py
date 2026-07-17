bf2582eac9e33a21c98ba866688ff514f535413b
SenaEnana <senaad47@gmail.com>
2026-07-17 16:37:25 +0300

    Append the valid position and push the new state to our stack

--- 11-python-graphs-and-trees/implement-n-queens-lab.py
+++ 11-python-graphs-and-trees/implement-n-queens-lab.py
@@ -16,9 +16,10 @@ def dfs_n_queens(n):
         for col in range(n - 1, -1, -1):
             is_valid = True
             for row_idx, col_idx in enumerate(current_state):
-                # 1. Same column check: col_idx == col
-                # 2. Diagonal checks: distance in rows equals distance in columns
                 if col_idx == col or abs(col_idx - col) == abs(row_idx - current_row):
                     is_valid = False
                     break
                     
+            if is_valid:
+                stack.append(current_state + [col])
+                
