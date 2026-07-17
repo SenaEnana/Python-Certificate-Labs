6719724ab7c661b5f1914b57cbf27f4e5dede2cd
SenaEnana <senaad47@gmail.com>
2026-07-17 10:06:05 +0300

    Create a function named shortest_path that takes in three parameters: matrix, start_node, and target_node. Assign None as the default value for target_node.

--- 11-python-graphs-and-trees/shortest-path-algorithm-workshop.py
+++ 11-python-graphs-and-trees/shortest-path-algorithm-workshop.py
@@ -1,3 +1,12 @@
 INF = float('inf')
-adj_matrix = [[0, 5, 3, INF, 11, INF], [5, 0, 1, INF, INF, 2], [3, 1, 0, 1, 5, INF],[INF, INF, 1, 0, 9, 3], [11, INF, 5, 9, 0, INF],
-[INF, 2, INF, 3, INF, 0]]
+adj_matrix = [
+    [0, 5, 3, INF, 11, INF],
+    [5, 0, 1, INF, INF, 2],
+    [3, 1, 0, 1, 5, INF],
+    [INF, INF, 1, 0, 9, 3],
+    [11, INF, 5, 9, 0, INF],
+    [INF, 2, INF, 3, INF, 0],
+]
+
+def shortest_path(matrix, start_node, target_node = None):
+    pass
