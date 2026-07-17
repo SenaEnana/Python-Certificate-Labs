751eaaba21229ecac5c515b5b3b23549732da065
SenaEnana <senaad47@gmail.com>
2026-07-17 08:09:44 +0300

    The hanoi_solver function should return a string with all the moves taken to solve the puzzle, including the starting arrangement, with each move on a new line.

--- 10-python-algorithms/implement-hanoi-tower-certification-project.py
+++ 10-python-algorithms/implement-hanoi-tower-certification-project.py
@@ -2,3 +2,6 @@ def hanoi_solver(n):
     # Initialize the three pegs:
     # Peg 0 starts with disks from n down to 1, while Peg 1 and Peg 2 are empty.
     pegs = [list(range(n, 0, -1)), [], []]
+    
+    states = []
+    
