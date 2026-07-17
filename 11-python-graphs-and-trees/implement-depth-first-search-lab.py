7032fdb0befeb4b2ae3049f1998464ddc33d38f4
SenaEnana <senaad47@gmail.com>
2026-07-17 16:23:57 +0300

    Continue processing as long as there are nodes in the stack

--- 11-python-graphs-and-trees/implement-depth-first-search-lab.py
+++ 11-python-graphs-and-trees/implement-depth-first-search-lab.py
@@ -3,3 +3,7 @@ def dfs(matrix, start_node):
     stack = [start_node]
     # Keep track of visited nodes to prevent infinite loops and track reachability
     visited = []
+    
+    # Continue processing as long as there are nodes in the stack
+    while stack:
+        pass
