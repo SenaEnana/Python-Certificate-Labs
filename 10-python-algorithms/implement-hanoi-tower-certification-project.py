3d69e76d7d2aadd8be488e4dc7b83b89217ef6a0
SenaEnana <senaad47@gmail.com>
2026-07-17 08:08:59 +0300

    The hanoi_solver function should solve the puzzle following the given rules in 2n - 1 moves, where n is the total number of disks.

--- 10-python-algorithms/implement-hanoi-tower-certification-project.py
+++ 10-python-algorithms/implement-hanoi-tower-certification-project.py
@@ -1,4 +1,4 @@
 def hanoi_solver(n):
     # Initialize the three pegs:
     # Peg 0 starts with disks from n down to 1, while Peg 1 and Peg 2 are empty.
-    pass
+    pegs = [list(range(n, 0, -1)), [], []]
