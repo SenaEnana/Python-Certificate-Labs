21fbca925888622794e4887de46365964083fae2
SenaEnana <senaad47@gmail.com>
2026-07-17 10:02:44 +0300

    Create a variable named adj_matrix and assign it a 2D list representing the graph with the following weights:

--- 11-python-graphs-and-trees/shortest-path-algorithm-workshop.py
+++ 11-python-graphs-and-trees/shortest-path-algorithm-workshop.py
@@ -1,1 +1,3 @@
-INF =float('inf')
+INF = float('inf')
+adj_matrix = [[0, 5, 3, INF, 11, INF], [5, 0, 1, INF, INF, 2], [3, 1, 0, 1, 5, INF],[INF, INF, 1, 0, 9, 3], [11, INF, 5, 9, 0, INF],
+[INF, 2, INF, 3, INF, 0]]
