314794d7760a34f985fce40fe9dc0c19c3fd4580
SenaEnana <senaad47@gmail.com>
2026-07-17 11:30:12 +0300

    The function should take a dictionary representing the adjacency list of an unweighted (either undirected or directed) graph as its argument

--- 11-python-graphs-and-trees/adjacency-list-matrix-converter-lab.py
+++ 11-python-graphs-and-trees/adjacency-list-matrix-converter-lab.py
@@ -1,2 +1,4 @@
 def adjacency_list_to_matrix(adj_list):
-    pass
+    # 1. Determine the number of nodes
+    # The number of nodes is equal to the number of keys in the adjacency list
+    num_nodes = len(adj_list)
