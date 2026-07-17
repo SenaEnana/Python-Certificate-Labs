65ecd8231cd1735e0dace36a5c7dd01389f3ab4b
SenaEnana <senaad47@gmail.com>
2026-07-17 09:08:09 +0300

    ready questions

---
+++ 11-python-graphs-and-trees/bfs-and-dfs-theory-exercise.py
@@ -0,0 +1,34 @@
+# Questions
+
+# Which of the following data structures is commonly used to implement a standard breadth-first search (BFS) algorithm?
+
+
+# Stack
+
+# Queue
+
+# Linked List
+
+# Tree
+
+# Which of the following statements about depth-first search (DFS) is true?
+
+
+# Depth-first search is guaranteed to find the shortest path between two nodes in an unweighted graph.
+
+# Depth-first search visits all neighbors at the current level before moving to the next level.
+
+# Depth-first search is always more space-efficient than BFS.
+
+# Depth-first search is typically implemented using recursion or a stack.
+
+# If you wanted to find the shortest path from a starting node to a target node in an unweighted graph, which algorithm would be the most suitable choice?
+
+
+# Breadth-first search (BFS)
+
+# Depth-first search (DFS)
+
+# Binary search
+
+# Merge sort
