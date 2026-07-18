0318c28673121c5b0248abea60672e01bff60b99
SenaEnana <senaad47@gmail.com>
2026-07-18 08:34:23 +0300

    with test cases

--- 12-dynamic-programming/nth-fibonnic-number-lab-instruction.py
+++ 12-dynamic-programming/nth-fibonnic-number-lab-instruction.py
@@ -9,3 +9,15 @@
 # Calling fibonacci(n) should use a dynamic programming approach to compute and return the n-th 
 # number from the Fibonacci sequence, where each number is the sum of the two preceding numbers.
 # Each computed Fibonacci number should be appended to the sequence list.
+# Tests:
+# Waiting:1. You should have a function named fibonacci.
+# Waiting:2. Your fibonacci function should take a single parameter.
+# Waiting:3. You should have a list named sequence within the fibonacci function initialized to [0, 1].
+# Waiting:4. fibonacci(0) should return 0.
+# Waiting:5. fibonacci(1) should return 1.
+# Waiting:6. fibonacci(2) should return 1.
+# Waiting:7. fibonacci(3) should return 2.
+# Waiting:8. fibonacci(5) should return 5.
+# Waiting:9. fibonacci(10) should return 55.
+# Waiting:10. fibonacci(15) should return 610.
+# Waiting:11. You should not use recursion in your code.
