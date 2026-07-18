e4d3b7461648eb9cb759cf7266d37b02af563f15
SenaEnana <senaad47@gmail.com>
2026-07-18 08:37:09 +0300

    Handle base cases if n is already within the initialized sequence

--- 12-dynamic-programming/nth-fibonic-number-lab.py
+++ 12-dynamic-programming/nth-fibonic-number-lab.py
@@ -1,4 +1,9 @@
-def fibonacci():
+def fibonacci(n):
     # 1. Initialize the sequence list exactly as required
     sequence = [0, 1]
+    
+    
+    if n < len(sequence):
+        return sequence[n]
+        
     
