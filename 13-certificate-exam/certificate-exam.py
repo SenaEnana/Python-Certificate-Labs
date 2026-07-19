26b7504509272c56734fb278f7fb27f662e8c38a
SenaEnana <senaad47@gmail.com>
2026-07-19 10:00:00 +0300

    added questions

--- 13-certificate-exam/certificate-exam.py
+++ 13-certificate-exam/certificate-exam.py
@@ -40,4 +40,31 @@
 
 # ValidName
 
+# 5 Context
+
+# my_list = [3.99, '42', True]
+
+# my_new_list = [int(i) for i in my_list]print(my_new_list)
+
+# What is the result of running the code above?
+
+# [3.99, 42, True]
+
+# TypeError
+
+# [3, 42, 1]
+
+# [4, 42, 1] 
+
+# 6 Which of the following is true about dictionary keys?
+
+# They must be strings.
+
+# They must be unique and immutable.
+
+# They cannot be boolean values.
+
+# They must be inserted in alphabetical order.
+
+
 #you can upload and view the exam questions if you are curious also with this am waiting for my certification for the result! thank you!
